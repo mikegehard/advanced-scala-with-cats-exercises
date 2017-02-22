ade2237e6300799c741117f0d9302fb95502c67c
Mike Gehard <mike@wundercapital.com>
2017-02-22 14:23:41 -0700

    reduce duplication

--- src/main/scala/advancedScala/chapter1/Printable.scala
+++ src/main/scala/advancedScala/chapter1/Printable.scala
@@ -30,7 +30,7 @@ object PrintSyntax {
   implicit class PrintOps[A](value: A) {
     def format(implicit p: Printable[A]): String = p.format(value)
 
-    def print(implicit p: Printable[A]): Unit = println(p.format(value))
+    def print(implicit p: Printable[A]): Unit = println(value.format)
   }
 
 }
