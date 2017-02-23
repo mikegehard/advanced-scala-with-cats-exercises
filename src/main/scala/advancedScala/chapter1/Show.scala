ee56e11ad618731bf329b4b90d23de1710c35ed8
Mike Gehard <mike@wundercapital.com>
2017-02-22 17:00:54 -0700

    Implement show using Cats.Show

--- src/main/scala/advancedScala/chapter1/Show.scala
+++ src/main/scala/advancedScala/chapter1/Show.scala
@@ -10,6 +10,17 @@ import cats.instances.string._
 // pull in
 import cats.syntax.show._
 
+object ShowableInstances {
+  implicit val catShow: Show[Cat] = {
+    //    Show.show(cat => s"${cat.name.show} is a ${cat.age.show} year-old ${cat.color.show} cat.")
+    //    val stringShow = Show[String]
+    //    val intShow = Show[Int]
+    //    Show.show(cat => s"${stringShow.show(cat.name)} is a ${intShow.show(cat.age)} year-old ${stringShow.show(cat.color)} cat.")
+    Show.show(cat => s"${Show[String].show(cat.name)} is a ${Show[Int].show(cat.age)} year-old ${Show[String].show(cat.color)} cat.")
+  }
+
+}
+
 object ShowMain {
   def main(args: Array[String]): Unit = {
     // if you go looking for the companion object in the Cats source,
@@ -30,5 +41,9 @@ object ShowMain {
     println("With extension methods...")
     println(123.show)
     println("abc".show)
+
+    import ShowableInstances._
+    val cat = Cat("Betty", 6, "grey")
+    println(Show[Cat].show(cat))
   }
 }
